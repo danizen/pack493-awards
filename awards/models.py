40f812cd36b8a411b83afe1ee688158b24d41a6c
Daniel Davis <dan@danizen.net>
2016-04-27 00:54:39 -0400

    Change to a ManyToManyField with through=

--- awards/models.py
+++ awards/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class Den(models.Model):
     '''
     Each Den has a leader who is a user who may administrate the Den,
-    a list of Scouts, and a list of potential Activities
+    a list of Scouts, and a list of potential Adventures
     '''
     name = models.CharField(max_length=64, unique=True)
 
 
-class Activity(models.Model):
+class Adventure(models.Model):
     '''
-    An Activity belongs to a Den. Each Activity may be required or an elective.
+    An Adventure belongs to a Den. Each Adventure may be required or optional.
     '''
     name = models.CharField(max_length=64, unique=True)
     den = models.ForeignKey(Den, on_delete=models.CASCADE)
@@ -20,21 +20,25 @@ class Activity(models.Model):
 
 class Scout(models.Model):
     '''
-    A Scout is a member of a Den, and earns Awards by performing Activities
+    A Scout is a member of a Den, and earns Awards by having Adventures
     '''
     name = models.CharField(max_length=64, unique=True)
     den = models.ForeignKey(Den, on_delete=models.CASCADE)
+    awards = models.ManyToManyField(Adventure,
+                                    through='Award',
+                                    through_fields=('scout', 'adventure'))
 
 
 class Award(models.Model):
     '''
-    A scout earns awards. These awards are earned, reported, purchased, and
-    delivered at particular times. Den masters mark the award as earned. The
-    awards coordinator reports the award and purchases the award. Then the Den
-    leaders deliver the award.
+    A scout earns Awards for completing Adventures. 
+    These awards are earned, reported, purchased, and delivered.
+    Den leaders mark the award as earned. 
+    The awards coordinator reports the award and purchases the award. 
+    Then the Den leaders deliver the award.
     '''
-    activity = models.OneToOneField(Activity, on_delete=models.CASCADE)
-    scout = models.OneToOneField(Scout, on_delete=models.CASCADE)
+    scout = models.ForeignKey(Scout, on_delete=models.CASCADE)
+    adventure = models.ForeignKey(Adventure, on_delete=models.CASCADE)
     earned = models.DateField(null=True)
     reported = models.DateField(null=True)
     purchased = models.DateField(null=True)
